6ed5bc0f379d97307ebdd0732c78bbe894ba1868
Clint Fralick <cfralick@creeperengine.com>
2015-11-17 14:17:05 -0500

    simplify barebones.js

--- assets/js/barebones.js
+++ assets/js/barebones.js
@@ -1,29 +1,9 @@
-;(function($, d, w, undefined) {
+;(function($, disqus_shortname, d, w, undefined) {
     "use strict";
     
     var $nav = $('header.navbar-fixed-top');
-    $(window).scroll({st: 0}, function(event) {
-        var sn = $(window).scrollTop();
-        (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
-        this.st = sn;
-    });
-
-    $.fn.bootstrap = function(options) {
-        var opts = $.extend({}, $.fn.bootstrap.defaults, options);
-        return this.each(function() {
-            for(var p in opts) {
-                $(p).addClass(opts[p]);
-            }
-        });
-    };
-            
-    $.fn.disqus = function(sn) {
-        return this.each(function() {
-            $(this).has($.fn.disqus.defaults.id)
-                .append($(getScript(sn)));
-        });
-    };
     
+
     var getScript = function(sn) {
         var s = d.createElement('script');
         s.async = true;
@@ -32,7 +12,15 @@
         return s;
     };
     
-    $.fn.disqus.defaults = {id: '#disqus_thread'};
-    $.fn.bootstrap.defaults = {pre: 'prettyprint', blockquote: 'blockquote'};
-    $('body').bootstrap().disqus(disqus_shortname);
-})(jQuery, document, window, undefined);
+    $(w).scroll({st: 0}, function(event) {
+        var sn = $(w).scrollTop();
+        (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
+        this.st = sn;
+    });
+    
+    $('pre').addClass('prettyprint');
+    $('blockquote').addClass('blockquote');
+    $('body').has('#disqus_thread')
+        .append($(getScript(disqus_shortname)));
+
+})(jQuery, disqus_shortname, document, window, undefined);
