5c26f92d909874c98bf73bfc63369655475ee532
Clint Fralick <cfralick@creeperengine.com>
2015-11-22 03:51:44 -0500

    simplify disqus loading

--- assets/js/barebones.js
+++ assets/js/barebones.js
@@ -1,30 +1,28 @@
 ;(function($, disqus_shortname, d, w, undefined) {
-    "use strict";
-    
-    var $nav = $('header.navbar-fixed-top');
-    
-    var disqus_config = function () {
-        this.page.url = location.href;
-        this.page.identifier = location.pathname;
-    };
+  "use strict";
+  
+  var disqusOptions = {
+    async: true,
+    src: '//'+disqus_shortname+'.disqus.com/embed.js',
+    data: { timestamp: +new Date() }
+  };
 
-    var getScript = function(sn) {
-        var s = d.createElement('script');
-        s.async = true;
-        s.src = '//'+sn+'.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        return s;
-    };
-    
-    $(w).scroll({st: 0}, function(event) {
-        var sn = $(w).scrollTop();
-        var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
-        this.st = sn;
-    });
-    
-    $('pre').addClass('prettyprint');
-    $('blockquote').addClass('blockquote');
-    $('body').has('#disqus_thread')
-        .append($(getScript(disqus_shortname)));
+  var disqus_config = function () {
+    this.page.url = location.href;
+    this.page.identifier = location.pathname;
+  };
+  
+  var $nav = $('header.later.navbar-fixed-top');
+  $(w).scroll({st: 0}, function(event) {
+    var sn = $(w).scrollTop();
+    var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
+    this.st = sn;
+  });
+
+  $('pre').addClass('prettyprint');
+  $('blockquote').addClass('blockquote');
+  
+  $('body').has('#disqus_thread')
+    .append($('<script/>', disqusOptions));
 
 })(jQuery, disqus_shortname, document, window, undefined);
