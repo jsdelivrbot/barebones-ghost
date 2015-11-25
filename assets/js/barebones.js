ee6cf4312554d1c205fddf55f50db4688121c21c
Clint Fralick <cfralick@creeperengine.com>
2015-11-25 12:13:07 -0500

    lazy-load disqus comments

--- assets/js/barebones.js
+++ assets/js/barebones.js
@@ -1,28 +1,40 @@
 ;(function($, disqus_shortname, d, w, undefined) {
   "use strict";
   
-  var disqusOptions = {
-    async: true,
-    src: '//'+disqus_shortname+'.disqus.com/embed.js',
-    data: { timestamp: +new Date() }
-  };
+  (function($, shortname, d, w) {
+    var DISQUS_ID = '#disqus_thread';
 
-  var disqus_config = function () {
-    this.page.url = location.href;
-    this.page.identifier = location.pathname;
-  };
+    var disqusOptions = {
+      async: true,
+      src: '//'+shortname+'.disqus.com/embed.js',
+      data: { timestamp: +new Date() }
+    };
+    
+    var disqus_config = function () {
+      this.page.url = location.href;
+      this.page.identifier = location.pathname;
+    };
   
-  var $nav = $('header.later.navbar-fixed-top');
-  $(w).scroll({st: 0}, function(event) {
-    var sn = $(w).scrollTop();
-    var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
-    this.st = sn;
-  });
+    $(w).one('scroll', function(event) {
+      $('body').has(DISQUS_ID)
+        .append($('<script/>', disqusOptions))
+        .find(DISQUS_ID)
+        .delay(500)
+        .fadeTo("slow", 1);
+    });
+  })($, disqus_shortname, d, w);
+
+ 
+  (function($, d, w) {
+    var $nav = $('header.later.navbar-fixed-top');
+  
+    $(w).scroll({st: 0}, function(event) {
+      var sn = $(w).scrollTop();
+      var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
+      this.st = sn;
+    });
+  })($, d, w);
 
   $('pre').addClass('prettyprint');
   $('blockquote').addClass('blockquote');
-  
-  $('body').has('#disqus_thread')
-    .append($('<script/>', disqusOptions));
-
 })(jQuery, disqus_shortname, document, window, undefined);
