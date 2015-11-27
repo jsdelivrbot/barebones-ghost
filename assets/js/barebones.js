7476a0a52c5b5b15a369c3467170e95a63e4d041
Clint Fralick <cfralick@creeperengine.com>
2015-11-27 10:34:20 -0500

    Add adjacent post links
    * Added next-post and previous-post links
    * Added tooltips
    * Added blog title in top navbar
    * Tightened up css/js

--- assets/js/barebones.js
+++ assets/js/barebones.js
@@ -1,40 +1,53 @@
-;(function($, disqus_shortname, d, w, undefined) {
+;(function($, shortname, d, w, undefined) {
   "use strict";
-  
-  (function($, shortname, d, w) {
-    var DISQUS_ID = '#disqus_thread';
 
-    var disqusOptions = {
+  var DISQUS_ID = '#disqus_thread';
+  var DISQUS_URL = ['//', shortname, '.disqus.com/embed.js'].join("");
+  var ADD_CLASSES = {
+    pre: 'prettyprint',
+    blockquote: 'blockquote'
+  };
+
+  var $nav = $('.navbar-fixed-top');
+
+  var addClasses = function() {
+    for(var c in ADD_CLASSES) {
+      $(c).addClass(ADD_CLASSES[c]);
+    }
+  };
+
+  var loadDisqus = function() {
+    return $('<script/>', {
       async: true,
-      src: '//'+shortname+'.disqus.com/embed.js',
+      src: DISQUS_URL,
       data: { timestamp: +new Date() }
-    };
-    
-    var disqus_config = function () {
-      this.page.url = location.href;
-      this.page.identifier = location.pathname;
-    };
-  
-    $(w).one('scroll', function(event) {
-      $(DISQUS_ID)
-        .append($('<script/>', disqusOptions))
-        .delay(800)
-        .fadeTo(null, 1);
     });
-  })($, disqus_shortname, d, w);
+  };
 
- 
-  (function($, d, w) {
-    var $nav = $('header.navbar-fixed-top');
+  var disqus_config = function () {
+    this.page.url = location.href;
+    this.page.identifier = location.pathname;
+  };
+
+  $(w).one('scroll', function(event) {
+    $('body').has(DISQUS_ID).append(loadDisqus());
+  });
   
-    $(w).scroll({st: 0}, function(event) {
-      var sn = $(w).scrollTop();
-      var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
-      this.st = sn;
-    });
-  })($, d, w);
+  $(w).one('scroll', function(event) {
+    $(DISQUS_ID).delay(800).fadeTo(null, 1);
+  });
 
-  $('pre').addClass('prettyprint');
-  $('blockquote').addClass('blockquote');
-  $('[data-toggle="tooltip"]').tooltip({placement: 'top', container: 'body'});
+  $(w).scroll({st: 0}, function(event) {
+    var sn = $(w).scrollTop();
+    var x = (sn < this.st) ? $nav.slideDown() : $nav.slideUp();
+    this.st = sn;
+  });
+
+  addClasses();
+
+  $('[data-toggle="tooltip"]').tooltip({
+    container: 'body',
+    placement: 'top'
+  });
+
 })(jQuery, disqus_shortname, document, window, undefined);
